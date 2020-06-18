311219a2f80f61fe5cf85d1a8a4ed36e4f69f2ce
Dmytro Kulyk <dmytro@dkulyk.name>
2020-06-18 16:25:11 +0300

    wip

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
+
 declare(strict_types=1);
 
 use Illuminate\Routing\Router;
 use RabbitCMS\Modules\Module;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 
 /* @var Router $router */
 
-Route::post('callback/{shop}', 'CallbackAction@handle')->name('callback');
+$router->post('callback/{shop}', 'CallbackAction@handle')->name('callback')
+    ->withoutMiddleware([VerifyCsrfToken::class, \App\Http\Middleware\VerifyCsrfToken::class]);
