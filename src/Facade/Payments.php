2076ce04cffa69b0814806aceff3ed719230a3f2
Dmytro Kulyk <lnkvisitor.ts@gmail.com>
2020-12-29 14:04:17 +0200

    wip

--- src/Facade/Payments.php
+++ src/Facade/Payments.php
@@ -1,20 +1,19 @@
 <?php
+
 declare(strict_types=1);
 
 namespace RabbitCMS\Payments\Facade;
 
 use Illuminate\Support\Facades\Facade;
+use RabbitCMS\Payments\Contracts\PaymentProviderInterface;
 
 /**
  * Class Payments
  *
- * @package RabbitCMS\Payments\Facade
+ * @method static PaymentProviderInterface driver(string $driver)
  */
 class Payments extends Facade
 {
-    /**
-     * @return string
-     */
     protected static function getFacadeAccessor(): string
     {
         return 'payments';
